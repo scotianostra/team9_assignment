79eae9d10da101b774b4d8ae75825a0ab29fab70
Aistis <smikislt@yahoo.com>
2016-03-09 22:55:28 +0000

    Up and down sort arrows implemented and  fully working over multiple columns in table

--- Timetabling/app/src/main/java/android/team9/com/timetabling/StaffModuleAttendanceActivity.java
+++ Timetabling/app/src/main/java/android/team9/com/timetabling/StaffModuleAttendanceActivity.java
@@ -9,68 +9,111 @@ import android.view.View;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class StaffModuleAttendanceActivity extends AppCompatActivity {
     private String moduleId;
+    private String JSON_URL = "http://api.ouanixi.com/";
+    private TableLayout tableLayout;
+    private TableRow headingRow;
+    private TreeMap<String, Integer> sortReminder;
+    private ArrayList<TextView> headers;
+    private ArrayList<String> defaultHeaders;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_staff_module_attendance);
+        moduleId = getIntent().getStringExtra("moduleId");
+        // int 0 neutral, 2 ascending, 1 descending
+        sortReminder = new TreeMap<>();
+        sortReminder.put((getString(R.string.matricNumber)), 0);
+        sortReminder.put((getString(R.string.forename)), 0);
+        sortReminder.put((getString(R.string.surname)), 0);
+        sortReminder.put((getString(R.string.attendance)), 0);
         init();
+        populateTable();
     }
 
     public void init() {
-        final TableLayout tableLayout = (TableLayout) findViewById(R.id.tableMain);
-        TableRow tableRow = new TableRow(this);
-        tableRow.setPadding(25, 25, 25, 25);
-
-        TextView tv0 = new TextView(this);
-        tv0.setText("    Matriculation Nr. ");
-        tv0.setTextColor(Color.WHITE);
-        tv0.setOnClickListener(new View.OnClickListener() {
+        tableLayout = (TableLayout) findViewById(R.id.tableMain);
+        headingRow = new TableRow(this);
+        headingRow.setPadding(25, 25, 25, 25);
+        headers = new ArrayList<>();
+        defaultHeaders = new ArrayList<>();
+        for (int i = 0; i < 4; i++) {
+            headers.add(new TextView(this));
+        }
+        defaultHeaders.add(getString(R.string.matricNumber));
+        headers.get(0).setText(R.string.matricNumber);
+        headers.get(0).setTextColor(Color.WHITE);
+        headers.get(0).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                headers.get(0).setText(changeSortDirection(0, headers.get(0).getText().toString()));
                 //sort based on matric nr here or do api magic here
             }
         });
-        tableRow.addView(tv0);
+        headingRow.addView(headers.get(0));
 
-        TextView tv1 = new TextView(this);
-        tv1.setText(" Forename ");
-        tv1.setTextColor(Color.WHITE);
-        tv1.setOnClickListener(new View.OnClickListener() {
+
+        // refactored to use string resources
+        headers.get(1).setText(R.string.forename);
+        defaultHeaders.add(getString(R.string.forename));
+        headers.get(1).setTextColor(Color.WHITE);
+        headers.get(1).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                headers.get(1).setText(changeSortDirection(1, headers.get(1).getText().toString()));
                 //sort based on forename here or do api magic here
             }
         });
 
-        tableRow.addView(tv1);
+        headingRow.addView(headers.get(1));
 
-        TextView tv2 = new TextView(this);
-        tv2.setText(" Surname ");
-        tv2.setTextColor(Color.WHITE);
-        tv2.setOnClickListener(new View.OnClickListener() {
+        headers.get(2).setText(R.string.surname);
+        defaultHeaders.add(getString(R.string.surname));
+        headers.get(2).setTextColor(Color.WHITE);
+        headers.get(2).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                headers.get(2).setText(changeSortDirection(2, headers.get(2).getText().toString()));
                 //sort based on surname here or do api magic here
             }
         });
-        tableRow.addView(tv2);
+        headingRow.addView(headers.get(2));
 
-        TextView tv3 = new TextView(this);
-        tv3.setText(" Attendance % ");
-        tv3.setTextColor(Color.WHITE);
-        tv3.setOnClickListener(new View.OnClickListener() {
+        headers.get(3).setText(R.string.attendance);
+        defaultHeaders.add(getString(R.string.attendance));
+        headers.get(3).setTextColor(Color.WHITE);
+        headers.get(3).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                headers.get(3).setText(changeSortDirection(3, headers.get(3).getText().toString()));
                 //sort based on attendance here or do api magic here
             }
         });
-        tableRow.addView(tv3);
+        headingRow.addView(headers.get(3));
 
-        tableLayout.addView(tableRow);
+        tableLayout.addView(headingRow);
+
+    }
+
+    // refactored code into separate methods to make it more portable/accommodate sorting
+    private void populateTable() {
         for (int i = 0; i < 25; i++) {
             TableRow tableRowInside = new TableRow(this);
             tableRowInside.setClickable(true);
@@ -116,5 +159,68 @@ public class StaffModuleAttendanceActivity extends AppCompatActivity {
             tableRowInside.addView(t4v);
             tableLayout.addView(tableRowInside);
         }
+    }
+
+    private void sendRequest() {
+        StringRequest stringRequest = new StringRequest(JSON_URL + moduleId,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            showJSON(response);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(StaffModuleAttendanceActivity.this, error.getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                });
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+    }
+
+    private void showJSON(String json) throws JSONException {
+        ///  ParseJSON pj = new ParseJSON(json);
+        // pj.parseJSONEnrolledStudents();
+        // sl = new CustomStudentList(this, ParseJSON.matricNumber, ParseJSON.email, ParseJSON.fName, ParseJSON.lName);
+        //lv.setAdapter(sl);
+    }
+
+    private String changeSortDirection(int header, String column) {
+        Log.i("column", column);
+        for (Map.Entry<String, Integer> entry : sortReminder.entrySet()) {
+            String key = entry.getKey();
+            Integer value = entry.getValue();
+            Log.i("Value before", entry.getValue().toString());
+            if (column.regionMatches(0, key, 0, column.length() - 2)) {
+                if (value == 0) {
+                    column = column.replace("✦", "▼");
+                }
+                if (value == 1) {
+                    Log.i("inside 1", column);
+                    column = column.replace("▼", "▲");
+
+                }
+                if (value == 2) {
+                    Log.i("inside 2", column);
+                    entry.setValue(-1);
+                    column = column.replace("▲", "✦");
+                }
+                value = entry.getValue();
+                value++;
+                entry.setValue(value);
+            } else entry.setValue(0);
+            Log.i("Value after", entry.getValue().toString());
+        }
+        for (int i = 0; i < 4; i++) {
+            if (header != i)
+                headers.get(i).setText(defaultHeaders.get(i));
+        }
+        return column;
     }
 }
