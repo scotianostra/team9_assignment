54b2b84b9e5bd37afd2b5ee981c86adfb40bc2ad
Alexander Grey <alexandergrey@hotmail.co.uk>
2016-03-13 22:27:10 +0000

    Removed unnecessary import

--- Timetabling/app/src/main/java/android/team9/com/timetabling/StaffModuleAttendanceActivity.java
+++ Timetabling/app/src/main/java/android/team9/com/timetabling/StaffModuleAttendanceActivity.java
@@ -22,7 +22,6 @@ import org.json.JSONException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
